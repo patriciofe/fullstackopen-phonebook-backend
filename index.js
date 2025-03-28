599334aca6f58c922251a206d63c35bf7e46793b
Patricio Flores <patriciofe@outlook.com>
2025-03-28 09:29:28 -0400

    Removed CORS from back end

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
